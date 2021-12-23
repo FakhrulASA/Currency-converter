3613a2e5172b8246cc2a73ff6491bd0ec1213df3
fakhrulasa <fakhrulasa@gmail.com>
2021-12-23 20:57:11 +0600

    Initial commit

---
+++ app/src/main/java/com/siddiqei/currencyconverter/feature/converter/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.siddiqei.currencyconverter.feature.converter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.siddiqei.currencyconverter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
